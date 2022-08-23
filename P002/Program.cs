8490d2595c0a033488bceee736d85505f67c50d8
«Denis» <tytdenis2@gmail.com>
2022-08-23 14:54:14 +0500

    add P002

---
+++ P002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int randomNumber = new Random().Next(100, 1000);
+
+int firstDigit = randomNumber / 100;
+int lastDigit = randomNumber % 10;
+
+System.Console.WriteLine($"{randomNumber} → {firstDigit}{lastDigit}");
