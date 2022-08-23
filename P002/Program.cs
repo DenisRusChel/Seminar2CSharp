cfddc34949a31d81eb34906e4ed2b1178b685cf2
«Denis» <tytdenis2@gmail.com>
2022-08-23 16:08:08 +0500

    add P002 and P003

--- P002/Program.cs
+++ P002/Program.cs
@@ -1,8 +1,14 @@
 ﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 
-int randomNumber = new Random().Next(100, 1000);
+// int randomNumber = new Random().Next(100, 1000);
 
-int firstDigit = randomNumber / 100;
-int lastDigit = randomNumber % 10;
+// int firstDigit = randomNumber / 100;
+// int lastDigit = randomNumber % 10;
 
-System.Console.WriteLine($"{randomNumber} → {firstDigit}{lastDigit}");
+// System.Console.WriteLine($"{randomNumber} → {firstDigit}{lastDigit}");
+
+int CetNumber(int number)
+{
+    return (number / 100 * 10 + number % 10);
+}
+System.Console.WriteLine(CetNumber(new Random().Next(100, 1000)));
--- P003/Program.cs
+++ P003/Program.cs
@@ -2,17 +2,44 @@
 // является ил второе число кратным пермоу. 
 // Если ивтрое число не кратно числу первому, то программа выводт остаток от деления.
 
-Console.WriteLine("Введите первое число: ");
-int numberA = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите первое число: ");
+// int numberA = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
-int numberB = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int numberB = int.Parse(Console.ReadLine());
 
-if(numberA%numberB == 0)
+// if(numberA%numberB == 0)
+// {
+// System.Console.WriteLine("кратно");
+// }
+// else
+// {
+//     System.Console.WriteLine($"не кратно, остаток {numberA%numberB}");
+
+
+// void Check(int a, int b)
+// {
+//     if (a % b == 0)
+//     {
+//         System.Console.WriteLine("кратно");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine($"не кратно, остаток: {a % b}");
+//     }
+// }
+// Check(17, 7);
+
+
+bool Check(int num)
 {
-System.Console.WriteLine("кратно");
+    if (num % 7 == 0 && num % 23 ==0)
+    {
+    return true;
 }
 else
 {
-    System.Console.WriteLine($"не кратно, остаток {numberA%numberB}");
+    return false;
 }
+}
+System.Console.WriteLine((Check(161)));
