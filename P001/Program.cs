68c559c20347b488d4b6cf7f8bea254f081d800f
«Denis» <tytdenis2@gmail.com>
2022-08-23 14:39:34 +0500

    add P001

---
+++ P001/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+
+int randomNumber = new Random().Next(10, 100);
+int firstDigit = randomNumber / 10;
+int secondDigit = randomNumber % 10;
+
+int max = firstDigit;
+
+if (max < secondDigit)
+{
+    max = secondDigit;
+}
+System.Console.WriteLine($"Максимальная цифра в числе: {randomNumber} → {max}");
